d1d4ec8cbdef7f250436adcf1f76050630de9fa8
John Dough <liketoaccess@gmail.com>
2021-11-01 22:03:53 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+# filename          : chatbot.py
+# description       : Talk to humans and make them feel good
+# author            : Warren
+# email             : warren@secmail.pro
+# date              : 03-06-1970
+# version           : v1.0
+# usage             : python chatbot.py
+# notes             :
+# license           : MIT
+# py version        : 3.9.7 (must run on 3.6 or higher)
+#==============================================================================
 from discord.ext import commands
 from prsaw import RandomStuff
 
